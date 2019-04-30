1489e585e52fd5a33f722c1c2723019e2c67ab13
Nikola <Nikola@DESKTOP-GTJG1JQ>
2019-04-30 14:32:44 +0200

    Kompletiran unos sporta

--- FootballApp/src/businessService/SportsMetode.java
+++ FootballApp/src/businessService/SportsMetode.java
@@ -12,15 +12,20 @@ private Session session = sf.openSession();
 
 public boolean ubaciSport (String sportsName) {
 	Sports sport = new Sports();
+	session.beginTransaction();
 	try {
 		sport.setName(sportsName);
 		session.save(sport);
 		session.getTransaction().commit();
 		System.out.println("Uspesan unos");
+		return true;
 	}catch (Exception e) {
 		session.getTransaction().rollback();
 		System.out.println("Neuspesan unos");
-	} return false;
+		return false;
+	} finally {
+		session.close();
+	}
 
  }
 }
